96b77cf20fcd3b1c3b1d30519f5f75e72f7e7ec3
Krishna Addepalli <coolkrishna31@gmail.com>
2024-05-31 17:27:39 +0200

    Add BoxedParser struct and add map, pred, zero_or_more, one_or_more functions to parser trait

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,41 @@ type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
 
 trait Parser<'a, Output> {
     fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+
+    fn map<F, NewOutput>(self, map_fn: F) -> BoxedParser<'a, NewOutput>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+        NewOutput: 'a,
+        F: Fn(Output) -> NewOutput + 'a,
+    {
+        BoxedParser::new(map(self, map_fn))
+    }
+
+    fn pred<F>(self, pred_fn: F) -> BoxedParser<'a, Output>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+        F: Fn(&Output) -> bool + 'a,
+    {
+        BoxedParser::new(pred(self, pred_fn))
+    }
+
+    fn zero_or_more(self) -> BoxedParser<'a, Vec<Output>>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+    {
+        BoxedParser::new(zero_or_more(self))
+    }
+
+    fn one_or_more(self) -> BoxedParser<'a, Vec<Output>>
+    where
+        Self: Sized + 'a,
+        Output: 'a,
+    {
+        BoxedParser::new(one_or_more(self))
+    }
 }
 
 /// Implement the Parser trait for any function that matches the signature of a parser.
@@ -155,9 +190,8 @@ where
     map(pair(parser1, parser2), |(_left, right)| right)
 }
 
-
 /// # One or More
-fn one_or_more<'a ,P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
 where
     P: Parser<'a, A>,
 {
@@ -186,13 +220,12 @@ where
         //     return Err(input);
         // };
 
-
-       // let mut result =  zero_or_more(parser).parse(input);
-       //
-       //  result.map(|(next_input, mut items)| {
-       //      items.insert(0, first_item);
-       //      (next_input, items)
-       //  })
+        // let mut result =  zero_or_more(parser).parse(input);
+        //
+        //  result.map(|(next_input, mut items)| {
+        //      items.insert(0, first_item);
+        //      (next_input, items)
+        //  })
     }
 }
 
@@ -264,14 +297,14 @@ fn space0<'a>() -> impl Parser<'a, Vec<char>> {
 /// * zero or more characters that are not a quote
 /// * followed by another quote
 fn quoted_string<'a>() -> impl Parser<'a, String> {
-    map(
-        right(
+    right(
+        match_literal("\""),
+        left(
+            zero_or_more(any_char.pred(|c| *c != '"')),
             match_literal("\""),
-            left(zero_or_more(pred(any_char, |c| *c != '"')),
-                 match_literal("\"")),
         ),
-        |chars| chars.into_iter().collect(),
     )
+    .map(|chars| chars.iter().collect())
 }
 
 fn attribute_pair<'a>() -> impl Parser<'a, (String, String)> {
@@ -287,16 +320,33 @@ fn element_start<'a>() -> impl Parser<'a, (String, Vec<(String, String)>)> {
 }
 
 fn single_element<'a>() -> impl Parser<'a, Element> {
-    map(
-        left(element_start(), match_literal("/>")),
-        |(name, attributes)| Element {
-            name,
-            attributes,
-            children: vec![],
-        },
-    )
+    left(element_start(), match_literal("/>")).map(|(name, attributes)| Element {
+        name,
+        attributes,
+        children: vec![],
+    })
 }
 
+struct BoxedParser<'a, Output> {
+    parser: Box<dyn Parser<'a, Output> + 'a>,
+}
+
+impl<'a, Output> BoxedParser<'a, Output> {
+    fn new<P>(parser: P) -> Self
+    where
+        P: Parser<'a, Output> + 'a,
+    {
+        BoxedParser {
+            parser: Box::new(parser),
+        }
+    }
+}
+
+impl<'a, Output> Parser<'a, Output> for BoxedParser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+        self.parser.parse(input)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -390,10 +440,13 @@ mod tests {
     #[test]
     fn attribute_parser() {
         assert_eq!(
-            Ok(("", vec![
-                ("one".to_string(), "1".to_string()),
-                ("two".to_string(), "2".to_string())
-            ])),
+            Ok((
+                "",
+                vec![
+                    ("one".to_string(), "1".to_string()),
+                    ("two".to_string(), "2".to_string())
+                ]
+            )),
             attributes().parse(" one=\"1\" two=\"2\"")
         );
     }
